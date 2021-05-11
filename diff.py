e0c39e735d6cabbfeaf936c1fe8d667360fc3e17
Zimin Park <48933016+ZiminPark@users.noreply.github.com>
2021-05-11 20:10:07 +0900

    q-learning

--- diff.py
+++ diff.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class QAgent():
     def __init__(self):
-        self.q_table = np.zeros((5, 7, 4)) # 마찬가지로 Q 테이블을 0으로 초기화
+        self.q_table = np.zeros((5, 7, 4))  # 마찬가지로 Q 테이블을 0으로 초기화
         self.eps = 0.9
 
     def select_action(self, s):
@@ -13,27 +13,28 @@ class QAgent():
         x, y = s
         coin = random.random()
         if coin < self.eps:
-            action = random.randint(0,3)
+            action = random.randint(0, 3)
         else:
-            action_val = self.q_table[x,y,:]
+            action_val = self.q_table[x, y, :]
             action = np.argmax(action_val)
         return action
 
     def update_table(self, transition):
         s, a, r, s_prime = transition
-        x,y = s
+        x, y = s
         next_x, next_y = s_prime
-        a_prime = self.select_action(s_prime) # S'에서 선택할 액션 (실제로 취한 액션이 아님)
-        # SARSA 업데이트 식을 이용
-        self.q_table[x,y,a] = self.q_table[x,y,a] + 0.1 * (r + self.q_table[next_x,next_y,a_prime] - self.q_table[x,y,a])
+        a_prime = self.select_action(s_prime)  # S'에서 선택할 액션 (실제로 취한 액션이 아님)
+        # Q러닝 업데이트 식을 이용
+        self.q_table[x, y, a] = self.q_table[x, y, a] + 0.1 * (
+                    r + np.amax(self.q_table[next_x, next_y, :]) - self.q_table[x, y, a])
 
     def anneal_eps(self):
-        self.eps -= 0.03
-        self.eps = max(self.eps, 0.1)
+        self.eps -= 0.01  # Q러닝에선 epsilon 이 좀더 천천히 줄어 들도록 함.
+        self.eps = max(self.eps, 0.2)
 
     def show_table(self):
         q_lst = self.q_table.tolist()
-        data = np.zeros((5,7))
+        data = np.zeros((5, 7))
         for row_idx in range(len(q_lst)):
             row = q_lst[row_idx]
             for col_idx in range(len(row)):
