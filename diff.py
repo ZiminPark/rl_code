dc9c2091f30662f6e647822696d78a36a31bd888
Zimin Park <48933016+ZiminPark@users.noreply.github.com>
2021-05-11 20:09:44 +0900

    sarsa

--- diff.py
+++ diff.py
@@ -5,39 +5,35 @@ import numpy as np
 
 class QAgent():
     def __init__(self):
-        self.q_table = np.zeros((5, 7, 4))  # q벨류를 저장하는 변수. 모두 0으로 초기화.
+        self.q_table = np.zeros((5, 7, 4)) # 마찬가지로 Q 테이블을 0으로 초기화
         self.eps = 0.9
-        self.alpha = 0.01
 
     def select_action(self, s):
-        # eps-greedy로 액션을 선택
+        # eps-greedy로 액션을 선택해준다
         x, y = s
         coin = random.random()
         if coin < self.eps:
-            action = random.randint(0, 3)
+            action = random.randint(0,3)
         else:
-            action_val = self.q_table[x, y, :]
+            action_val = self.q_table[x,y,:]
             action = np.argmax(action_val)
         return action
 
-    def update_table(self, history):
-        # 한 에피소드에 해당하는 history를 입력으로 받아 q 테이블의 값을 업데이트 한다
-        cum_reward = 0
-        for transition in history[::-1]:
-            s, a, r, s_prime = transition
-            x, y = s
-            # 몬테 카를로 방식을 이용하여 업데이트.
-            self.q_table[x, y, a] = self.q_table[x, y, a] + self.alpha * (cum_reward - self.q_table[x, y, a])
-            cum_reward = cum_reward + r
+    def update_table(self, transition):
+        s, a, r, s_prime = transition
+        x,y = s
+        next_x, next_y = s_prime
+        a_prime = self.select_action(s_prime) # S'에서 선택할 액션 (실제로 취한 액션이 아님)
+        # SARSA 업데이트 식을 이용
+        self.q_table[x,y,a] = self.q_table[x,y,a] + 0.1 * (r + self.q_table[next_x,next_y,a_prime] - self.q_table[x,y,a])
 
     def anneal_eps(self):
         self.eps -= 0.03
         self.eps = max(self.eps, 0.1)
 
     def show_table(self):
-        # 학습이 각 위치에서 어느 액션의 q 값이 가장 높았는지 보여주는 함수
         q_lst = self.q_table.tolist()
-        data = np.zeros((5, 7))
+        data = np.zeros((5,7))
         for row_idx in range(len(q_lst)):
             row = q_lst[row_idx]
             for col_idx in range(len(row)):
